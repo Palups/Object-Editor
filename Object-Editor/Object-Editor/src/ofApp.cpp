7df791e0c1e187b53ab1ae231d8d166a1321832b
Vinícius Guilherme Palu <vinicius.palu@live.com>
2017-04-17 18:37:16 -0300

    testes para digitar nome do arquivo - ignorar

--- Object-Editor/Object-Editor/src/ofApp.cpp
+++ Object-Editor/Object-Editor/src/ofApp.cpp
@@ -31,7 +31,10 @@ void ofApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
+	string nomeDoArquivo;
 
+	if (key == 'n')
+		nomeDoArquivo = ofSystemTextBoxDialog("Nome do arquivo:", nomeDoArquivo);
 }
 
 //--------------------------------------------------------------
