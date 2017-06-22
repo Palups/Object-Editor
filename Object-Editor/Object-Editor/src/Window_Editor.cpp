4a9df039d87998f39146f21af0b227a68f6a0fad
Vinícius Guilherme Palu <vinicius.palu@live.com>
2017-06-22 00:39:58 -0300

    objtype agora de 0 a 2, n de 1 a 3

--- Object-Editor/Object-Editor/src/Window_Editor.cpp
+++ Object-Editor/Object-Editor/src/Window_Editor.cpp
@@ -3,7 +3,7 @@
 Window_Editor::Window_Editor()
 {
 	m_imageOnScreen = false; //se imagem está na tela
-	m_sliderControl = 1;
+	m_sliderControl = 0;
 	label_type = "Buffs";
 
 	//-----------------------------------------------------------------------------------------
@@ -92,13 +92,13 @@ void Window_Editor::SetLabelType()
 {
 	switch (m_sliderControl)
 	{
-	case 1:
+	case 0:
 		label_type = "Buffs";
 		break;
-	case 2:
+	case 1:
 		label_type = "Debuffs";
 		break;
-	case 3:
+	case 2:
 		label_type = "Obstacles";
 		break;
 	default:
@@ -123,7 +123,7 @@ void Window_Editor::KeyPressed(int key)
 void Window_Editor::MouseReleased(int x, int y)
 {
 	switch (m_sliderControl) {
-	case 1:
+	case 0:
 		if (s_protection->TestClick(x, y) && sw_protection->GetStatus()) //se o switch de objeto destrutivel estiver ativo
 			s_protection->MouseReleased(x, y);
 
@@ -141,7 +141,7 @@ void Window_Editor::MouseReleased(int x, int y)
 
 		break;
 
-	case 2:
+	case 1:
 		if (s_lessHP->TestClick(x, y) && sw_lessHP->GetStatus())
 			s_lessHP->MouseReleased(x, y);
 
@@ -153,7 +153,7 @@ void Window_Editor::MouseReleased(int x, int y)
 
 		break;
 
-	case 3:
+	case 2:
 		if (s_duration->TestClick(x, y) && sw_obstacle->GetStatus())
 			s_duration->MouseReleased(x, y);
 
@@ -183,7 +183,7 @@ void Window_Editor::MouseReleased(int x, int y)
 void Window_Editor::MouseDragged(int x, int y)
 {
 	switch (m_sliderControl) {
-	case 1:
+	case 0:
 		if (s_protection->TestClick(x, y) && sw_protection->GetStatus()) //se o switch de objeto destrutivel estiver ativo
 			s_protection->MouseDragged(x, y);
 
@@ -201,7 +201,7 @@ void Window_Editor::MouseDragged(int x, int y)
 
 		break;
 
-	case 2:
+	case 1:
 		if (s_lessHP->TestClick(x, y) && sw_lessHP->GetStatus())
 			s_lessHP->MouseDragged(x, y);
 
@@ -213,7 +213,7 @@ void Window_Editor::MouseDragged(int x, int y)
 
 		break;
 
-	case 3:
+	case 2:
 		if (s_duration->TestClick(x, y) && sw_obstacle->GetStatus())
 			s_duration->MouseDragged(x, y);
 
@@ -227,8 +227,8 @@ void Window_Editor::MousePressed(int x, int y, Window_Manager * window_manager)
 {
 	if (btn_changeSliders->TestClick(x, y)) { //alterar sliders que são vistos na tela
 		m_sliderControl++;
-		if (m_sliderControl == 4) {
-			m_sliderControl = 1;
+		if (m_sliderControl == 3) {
+			m_sliderControl = 0;
 		}
 
 		ResetSwitchesAndSliders();
@@ -321,7 +321,7 @@ void Window_Editor::MousePressed(int x, int y, Window_Manager * window_manager)
 
 		/*-----  ATRIBUTOS  -----*/
 		switch (m_sliderControl) {
-		case 1:
+		case 0:
 			//proteção
 			if (sw_protection->TestClick(x, y)) {
 				sw_protection->MouseClicked(x, y);
@@ -379,7 +379,7 @@ void Window_Editor::MousePressed(int x, int y, Window_Manager * window_manager)
 
 			break;
 
-		case 2:
+		case 1:
 			//tirar vida
 			if (sw_lessHP->TestClick(x, y)) {
 				sw_lessHP->MouseClicked(x, y);
@@ -415,7 +415,7 @@ void Window_Editor::MousePressed(int x, int y, Window_Manager * window_manager)
 
 			break;
 
-		case 3:
+		case 2:
 			//tirar ataque
 			if (sw_obstacle->TestClick(x, y)) {
 				sw_obstacle->MouseClicked(x, y);
@@ -463,7 +463,7 @@ void Window_Editor::Draw()
 		}
 
 		switch (m_sliderControl) {
-		case 1:
+		case 0:
 			s_protection->Draw();
 			sw_protection->Draw();
 			s_heal->Draw();
@@ -475,7 +475,7 @@ void Window_Editor::Draw()
 			s_time->Draw();
 			sw_time->Draw();
 			break;
-		case 2:
+		case 1:
 			s_lessHP->Draw();
 			sw_lessHP->Draw();
 			s_lessSpeed->Draw();
@@ -483,7 +483,7 @@ void Window_Editor::Draw()
 			s_lessAttack->Draw();
 			sw_lessAttack->Draw();
 			break;
-		case 3:
+		case 2:
 			s_duration->Draw();
 			sw_obstacle->Draw();
 			break;
